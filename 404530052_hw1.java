fb9f6595929e0640a321fe0df2b255c3443ab30a
a0988986570 <a0988986570@gmail.com>
2016-03-15 01:20:49 +0800

    Added files via upload

---
+++ 404530052_hw1.java
@@ -0,0 +1,28 @@
+
+public class rectangle {
+	public static void main(String[] args){
+	
+	double  x=15,
+			y=25, 
+	        width=57,
+	        height=18,
+	        area=width*height,
+	        perimeter=(width+height)*2;
+	
+	System.out.println("java.Rectangle[X=" +x+ "," +"Y=" +y+ "," +"Width=" +width+ "," +"Height" +height+"]");
+    System.out.println("Area="+area);
+    System.out.println("Perimeter="+perimeter);
+    
+    double  x1=0,
+			y1=12, 
+	        width1=60,
+	        height1=47,
+	        area1=width1*height1,
+	        perimeter1=(width1+height1)*2;
+   System.out.println("java.Rectangle[X=" +x1+ "," +"Y=" +y1+ "," +"Width=" +width1+ "," +"Height" +height1+"]"); 
+   System.out.println("Area1="+area1);
+   System.out.println("Perimeter1="+perimeter1); 
+   
+   System.out.println("End of Output!");
+    }
+}
